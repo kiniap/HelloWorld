4d0524838cb353a791c95adee82ea0138bd57d3d
Ananth Kini <kiniap@gmail.com>
2013-04-14 13:12:12 -0500

    my first commit

---
+++ src/HelloWorld.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Hello.cpp
+// Author      : Ananth Kini
+// Version     : 1.0
+// Copyright   : GNU / GPL3.0
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "This is the first time I am using Eclipse on Windows" << endl; // prints !!!Hello World!!!
+	return 0;
+}
